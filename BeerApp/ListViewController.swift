235bb46eda1dadafb97bc19dcbfcb9243c7957e7
Pedro <pedro8azevedo@gmail.com>
2021-01-23 11:59:43 -0300

    Removed storyboard

--- BeerApp/ListViewController.swift
+++ BeerApp/ListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ListViewController.swift
 //  BeerApp
 //
 //  Created by Pedro Azevedo on 23/01/21.
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .purple
     }
 
 
